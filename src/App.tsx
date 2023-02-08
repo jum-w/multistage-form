73c3a88bae04eed324deacbda7fd06a720994a1c
jum <grayd647@gmail.com>
2023-02-08 23:19:26 +0000

    map through stages instead of component spam

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@ import Info from './components/Info';
 import Selection from './components/Selection'
 
 function App() {
-  const Selections = {
-    steps: [1, 2, 3, 4],
-    title: ["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"]
-  }
+  const selectionData = [
+    { step: 1, title: "YOUR INFO" },
+    { step: 2, title: "SELECT PLAN" },
+    { step: 3, title: "ADD-ONS" },
+    { step: 4, title: "SUMMARY" },
+  ]
 
   return (
     <div className="bg-blue-100 min-h-screen justify-center flex items-center font-ubuntu text-sm">
       <div className="bg-white w-defw h-defh flex rounded-xl">
         <div className="bg-blue-700 w-1/3 m-3 rounded-xl">
-          <Selection step={Selections.steps[0]} title={Selections.title[0]} />
-          <Selection step={Selections.steps[1]} title={Selections.title[1]} />
-          <Selection step={Selections.steps[2]} title={Selections.title[2]} />
-          <Selection step={Selections.steps[3]} title={Selections.title[3]} />
+          {selectionData.map((selection) => (
+            <Selection key={selection.step} step={selection.step} title={selection.title} />
+          ))}
         </div>
         <div className="w-2/3 m-3">
           <Info />
--- src/components/Selection.tsx
+++ src/components/Selection.tsx
@@ -5,7 +5,7 @@ interface SelectionProps {
 
 export default function Selection({ step, title }: SelectionProps) {
     return (
-        <div className="flex mx-8 my-6 items-center text-white font-bold">
+        <div className="flex ml-8 mr-4 my-6 items-center text-white font-bold">
             <div className="border-2 rounded-full px-3 items-center flex h-9">{step}</div>
             <div className="mx-4">
                 <label className="font-light text-gray-300">STEP {step}</label>
